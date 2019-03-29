5ea23860598590e873bd405c175ac5dd61221c3e
VadimDyshkant <serv@sent.com>
2019-03-29 21:31:29 +0200

    testing modules work

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+import AppService from './modules/app.service'
+// importing ES6 module
+import {config} from './modules/config'
+import './modules/header.component'
+
+
+
+console.log('Config key: ', config.key);
+
+const service = new AppService('Hello, buddy!');
+service.log()
